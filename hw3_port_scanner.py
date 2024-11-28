e74fe96008db2a87f282fd7ae546b0ddb03c0d2d
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2024-11-28 20:06:08 +0500

    Update hw3_port_scanner.py

--- hw3_port_scanner.py
+++ hw3_port_scanner.py
@@ -1,20 +1,17 @@
 import argparse
-import sys
 import time
-import traceback
+from concurrent.futures import ThreadPoolExecutor
 from enum import IntEnum
-from random import randint
-from socket import socket
 import logging
 
 from scapy.layers.inet import TCP, IP
 from scapy.packet import Packet
-from scapy.sendrecv import sr1, sr
+from scapy.sendrecv import sr1
 
 TIMEOUT = 2
 THREADS_NUMBER = 10
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 reserved_ports = {
     7: "ECHO",
@@ -42,13 +39,14 @@ def main():
     ip_address = args.ip_address
     ports = args.ports
     timeout = args.timeout if args.timeout else TIMEOUT
-    num_threads = args.num_threads if args.num_threads else THREADS_NUMBER
+    threads_num = args.num_threads if args.num_threads else THREADS_NUMBER
     guess = args.guess
     verbose = args.verbose
 
     tcp_ports, udp_ports = _get_tcp_and_udp_ports_sets(ports)
 
-    _print_and_open_tcp_ports(guess, ip_address, tcp_ports, timeout, verbose)
+    _print_open_tcp_ports(ip_address, tcp_ports, timeout, threads_num, verbose, guess)
+    _print_open_udp_ports(ip_address, udp_ports, timeout, threads_num, verbose, guess)
 
 
 def _construct_args_parser():
@@ -84,25 +82,38 @@ def _parse_port(port, ports):
             ports.add(int(part))
 
 
-def _print_and_open_tcp_ports(guess, ip_address, tcp_ports, timeout, verbose):
-    for port in tcp_ports:
-        logging.debug(f"Scanning TCP port {port}")
-        start = time.time()
-        response = _handle_tcp_port(port, ip_address, timeout)
-        time_ms = time.time() - start
-        if response == Responses.OPEN:
-            res = ["TCP", str(port)]
-            if verbose:
-                res.append(f"{time_ms:.2f}ms")
-            if guess:
-                res.append(reserved_ports.get(port, '-'))
-            print(" ".join(res))
+def _print_open_udp_ports(ip_address, udp_ports, timeout, threads_num, verbose, guess):
+    pass
+
+
+def _print_open_tcp_ports(ip_address, tcp_ports, timeout, threads_num, verbose, guess):
+    with ThreadPoolExecutor(max_workers=threads_num) as executor:
+        futures = [
+            executor.submit(_scan_and_print, port, ip_address, timeout, verbose, guess)
+            for port in tcp_ports
+        ]
+        for future in futures:
+            future.result()
+
+
+def _scan_and_print(port, ip_address, timeout, verbose, guess):
+    start = time.time()
+    logging.debug(f"Scanning port {port}")
+    response = _handle_tcp_port(port, ip_address, timeout)
+    time_ms = time.time() - start
+    if response == Responses.OPEN:
+        res = ["TCP", str(port)]
+        if verbose:
+            res.append(f"{time_ms:.2f}ms")
+        if guess:
+            res.append(reserved_ports.get(port, '-'))
+        print(" ".join(res))
 
 
 def _handle_tcp_port(dport, ip_address, timeout):
     ip = IP(dst=ip_address)
     port = TCP(dport=dport, flags="S")
-    reset = TCP(dport=dport, flags="F")
+    reset = TCP(dport=dport, flags="R")
     packet: Packet = ip / port
     reset_packet: Packet = ip / reset
 
